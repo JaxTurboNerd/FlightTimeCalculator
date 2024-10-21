2e4a4e623acb4284d276a66e49c6e932cbd8970e
Gregory Boyd <gboyd69@yahoo.com>
2024-10-20 21:53:39 -0400

    Added basic two tab view with 3 or 4 pilots.

--- FlightTimeCalculator/ContentView.swift
+++ FlightTimeCalculator/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Text("3 Pilots")
+                .tabItem {
+                    Label("3 Pilots", image: ._3Person)
+                }
+            Text("4 Pilot")
+                .tabItem {
+                    //Image(._4Person)
+                    Label("4 Pilots", image: ._4Person)
+                }
         }
-        .padding()
     }
 }
 
