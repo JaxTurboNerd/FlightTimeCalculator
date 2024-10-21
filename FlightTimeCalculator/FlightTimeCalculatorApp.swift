df419213912d1639adedc92646c71f7057f25bf4
Gregory Boyd <gboyd69@yahoo.com>
2024-10-20 21:16:10 -0400

    Initial Commit

---
+++ FlightTimeCalculator/FlightTimeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTimeCalculatorApp.swift
+//  FlightTimeCalculator
+//
+//  Created by Gregory Boyd on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FlightTimeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlightTimeCalculatorTests/FlightTimeCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTimeCalculatorTests.swift
+//  FlightTimeCalculatorTests
+//
+//  Created by Gregory Boyd on 10/20/24.
+//
+
+import Testing
+@testable import FlightTimeCalculator
+
+struct FlightTimeCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
